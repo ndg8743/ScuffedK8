11735a9d590a2cf26d0641a19cde2bce24aa66d7
Nathan Gopee <ndg8743@gmail.com>
2025-10-09 12:33:14 -0400

    fix: add main function to multithread demo

--- examples/multithread-demo/main.go
+++ examples/multithread-demo/main.go
@@ -14,21 +14,30 @@ func runNode(nodeID int, podQueue chan int) {
 	}
 }
 
-func runMultithreadExample() {
-   // Creating a channel
-   podQueue := make(chan int)
+func main() {
+	fmt.Println("=== Multithread Demo: 10,000 Nodes executing 100,000 Pods ===\n")
 
-   // Creating 10.000 workers to execute the task
-   for i := 0; i < 10000; i++ {
-	  go runNode(i, podQueue)
-   }
+	// Create a channel for distributing pods to nodes
+	podQueue := make(chan int)
 
-   // In real K8s, these would be containers (nginx, postgres, etc.)
-   numPods := 100000
-   for i := 0; i < numPods; i++ {
-	  podQueue <- i // Scheduler assigns pod to available node
-   }
+	// Create 10,000 NODES (worker computers)
+	// In real K8s, this would be like 10,000 servers/VMs
+	numNodes := 10000
+	fmt.Printf("Starting %d worker nodes...\n", numNodes)
+	for i := 0; i < numNodes; i++ {
+		go runNode(i, podQueue)
+	}
 
-   // Keep main alive to see results
-   time.Sleep(5 * time.Second)
+	// Create 100,000 PODS (applications/tasks to run)
+	// In real K8s, these would be containers (nginx, postgres, etc.)
+	numPods := 100000
+	fmt.Printf("Distributing %d pods to nodes...\n\n", numPods)
+	for i := 0; i < numPods; i++ {
+		podQueue <- i // Scheduler assigns pod to available node
+	}
+
+	// Keep main alive to see results
+	fmt.Println("\nWaiting for pods to complete...")
+	time.Sleep(10 * time.Second)
+	fmt.Println("Done!")
 }
